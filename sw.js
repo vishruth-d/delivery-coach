95e39317def9314d4a9826c0eb401f98da5def33
Vishruth <vishy.dhamo@gmail.com>
2026-03-16 22:22:24 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'delivery-coach-v5';
+const CACHE_NAME = 'delivery-coach-v7';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
